13b7822a9ae3960c5070aab3d3d71b14f32b1a25
13mei <mamaev.n@promail366.com>
2023-08-09 09:57:09 +0300

    FEAT: added opportunity upload img

--- app1 - ToDo/bonus/19_camera_in_web.py
+++ app1 - ToDo/bonus/19_camera_in_web.py
@@ -6,10 +6,19 @@ with st.expander('Start Camera'):
     camera = st.camera_input('Input')
     print(camera)
 
-if camera:
-    #Create pillow image instanse
-    img = Image.open(camera)
-    #Convert image
-    gray_img = img.convert('L')
-    #Render image to landscape
-    st.image(gray_img)
+    if camera:
+        #Create pillow image instanse
+        img = Image.open(camera)
+        #Convert image
+        gray_img = img.convert('L')
+        #Render image to landscape
+        st.image(gray_img)
+
+with st.expander('Upload and convert app'):
+    uploaded_image = st.file_uploader("Upload Image")
+    if uploaded_image:
+        img = Image.open(uploaded_image)
+        # Convert image
+        gray_img = img.convert('L')
+        # Render image to landscape
+        st.image(gray_img)
